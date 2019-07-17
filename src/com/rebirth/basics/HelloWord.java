47444751a099bc625ab0643b79bc7186fa5ba6f6
Administrator <1239490639@qq.com>
2019-07-17 22:05:07 +0800

    从hell word开始

---
+++ src/com/rebirth/basics/HelloWord.java
@@ -0,0 +1,10 @@
+package com.rebirth.basics;
+
+/**
+ * 从今天开始努力的重新学习代码，从hello word开始
+ */
+public class HelloWord {
+    public static void main(String[] args) {
+        System.out.println("Hello word");
+    }
+}
